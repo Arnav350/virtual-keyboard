eaf4b9305df100e48a8beb17f7e79bce34ebb7cb
Arnav Patel <patelarnavm+github@gmail.com>
2023-02-22 09:49:10 -0500

    added keys

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <input type="text" className="input" />
+      <div className="keyboard">
+        <div className="row">
+          <div className="key">`</div>
+          <div className="key">1</div>
+          <div className="key">2</div>
+          <div className="key">3</div>
+          <div className="key">4</div>
+          <div className="key">5</div>
+          <div className="key">6</div>
+          <div className="key">7</div>
+          <div className="key">8</div>
+          <div className="key">9</div>
+          <div className="key">0</div>
+          <div className="key">-</div>
+          <div className="key">=</div>
+          <div className="key key__backspace">{"<"}--</div>
+        </div>
+        <div className="row">
+          <div className="key key__tab">tab</div>
+          <div className="key">q</div>
+          <div className="key">w</div>
+          <div className="key">e</div>
+          <div className="key">r</div>
+          <div className="key">t</div>
+          <div className="key">y</div>
+          <div className="key">u</div>
+          <div className="key">i</div>
+          <div className="key">o</div>
+          <div className="key">p</div>
+          <div className="key">{"["}</div>
+          <div className="key">{"]"}</div>
+          <div className="key key__backslash">\</div>
+        </div>
+        <div className="row">
+          <div className="key key__caps">Caps Lock</div>
+          <div className="key">a</div>
+          <div className="key">s</div>
+          <div className="key">d</div>
+          <div className="key">f</div>
+          <div className="key">g</div>
+          <div className="key">h</div>
+          <div className="key">j</div>
+          <div className="key">k</div>
+          <div className="key">l</div>
+          <div className="key">;</div>
+          <div className="key">'</div>
+          <div className="key key__enter">Enter</div>
+        </div>
+        <div className="row">
+          <div className="key key__shift">Shift</div>
+          <div className="key">z</div>
+          <div className="key">x</div>
+          <div className="key">c</div>
+          <div className="key">v</div>
+          <div className="key">b</div>
+          <div className="key">n</div>
+          <div className="key">m</div>
+          <div className="key">,</div>
+          <div className="key">.</div>
+          <div className="key">/</div>
+          <div className="key key__shift">Shift</div>
+        </div>
+        <div className="row">
+          <div className="key__space"></div>
+        </div>
+      </div>
     </div>
   );
 }
